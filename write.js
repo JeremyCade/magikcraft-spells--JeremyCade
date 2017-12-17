7e6e71e147906fd8b14cd74aad12dbae9b812220
JeremyCade <me@jeremycade.com>
2017-12-17 04:49:00 +0000

    Update write.js

--- write.js
+++ write.js
@@ -7,7 +7,6 @@ function write() {
         c:[255,192,192,192,192,192,192,255],
         d:[256,195,195,195,195,195,195,256]
     };
-}
 
 function binary(letter) {
     return letter.map(line => line.toString(2));
@@ -15,3 +14,5 @@ function binary(letter) {
 
 const a = binary (font.a);
 a.forEach(line => maigk.dixit(line.padStart("0", 8)));
+
+}
